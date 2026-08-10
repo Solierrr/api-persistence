2d06617f6b1a6c1e0db13c86a8c897ef4e5c4dee
Eduardo Rodrigues Silva <eduardoultimate29@gmail.com>
2026-08-10 07:39:08 -0300

    refactor: directory name convention (#13)
    Co-authored-by: EduardoRSilva5102 <eduardoultimate29@gmail.com.br>

--- src/main/java/com/solaria/persistence/DTO/Request/SupplierSearchFilterDTO.java
+++ src/main/java/com/solaria/persistence/DTO/Request/SupplierSearchFilterDTO.java
@@ -1,6 +1,6 @@
-package com.solaria.persistence.dto;
+package com.solaria.persistence.dto.request;
 
-public class SupplierSearchFilter {
+public class SupplierSearchFilterDTO {
     // TODO: definir os filtros de pesquisa de fornecedores.
     // Esses campos serão utilizados posteriormente na geração das chaves de cache do Redis.
 }
--- src/main/java/com/solaria/persistence/util/CacheKeyGenerator.java
+++ src/main/java/com/solaria/persistence/util/CacheKeyGenerator.java
@@ -1,17 +1,15 @@
 package com.solaria.persistence.util;
 
 public final class CacheKeyGenerator {
-    private static final String PREFIX = "core"; 
-    // como vai ser implementado tanto no chatbot quanto mobile, o certo é separar entre "core" e "agent".
+    private static final String PREFIX = "core";
 
-    private CacheKeyGenerator() { // Construtor vazio pois ele não permite que seja criado nenhum objeto, apenas métodos
-    }
+    private CacheKeyGenerator() {}
 
-    public static String supplierSearch(String filters) { // Pesquisa de fornecedores por meio de filtros
+    public static String supplierSearch(String filters) {
         return PREFIX + ":search:supplier:" + filters;
     }
 
-    public static String searchCounter(String filters) {  // Contador de pesquisas, define o que é armazenado no Redis com base em popularidade
+    public static String searchCounter(String filters) {
         return PREFIX + ":counter:search:" + filters;
     }
 }
